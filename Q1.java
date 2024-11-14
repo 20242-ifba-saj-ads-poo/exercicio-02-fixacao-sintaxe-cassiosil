03432b59846d163ff140f06c2a92747e790abdd7
cassiosil <129911362+cassiosil@users.noreply.github.com>
2024-11-14 23:03:19 +0000

    q1

---
+++ Q1.java
@@ -0,0 +1,8 @@
+class ExercicioX {
+    public static void main(String[] args) {
+        public int num;
+        for(num = 150; num<=300;num++){
+            System.out.println(num);
+        }
+    }
+}
---
+++ Q2.java
@@ -0,0 +1,8 @@
+class ExercicioX {
+    public static void main(String[] args) {
+        public int mum, soma;
+        for(num=1;num<=1000;num++){
+            soma+=num;
+        }    
+    }
+}
